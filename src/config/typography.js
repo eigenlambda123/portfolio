c0472c742887e22831ff0839cdec2d11339f9020
eigenlambda123 <rmvilla987@gmail.com>
2026-09-10 20:28:45 +0800

    feat: Added a typography config so that the font is easily changeable

---
+++ src/config/typography.js
@@ -0,0 +1,4 @@
+export const typography = {
+  // Change this stack to update the font used throughout the site.
+  fontFamily: "Georgia, 'Times New Roman', serif",
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { typography } from './config/typography';
 import './styles.css';
 import 'katex/dist/katex.min.css';
+
+document.documentElement.style.setProperty('--font-family', typography.fontFamily);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
